a62506e7b01ae1251e81d7e7cf80fdb4102b81ea
liuw918 <yjwfn5656131>
2018-11-14 22:58:44 +0800

    环境配置和创建子应用

---
+++ meiduo_mall/meiduo_mall/utils/exceptions.py
@@ -0,0 +1,27 @@
+import logging
+
+from rest_framework.response import Response
+from rest_framework.views import exception_handler as drf_exception_handler
+from django.db import DatabaseError
+from redis.exceptions import RedisError
+
+logger = logging.getLogger("django")
+
+
+def exception_handler(exc, context):
+    # 1.drf 处理异常
+    # 2. 自己处理异常
+    """
+    :param exc: 异常的对象
+    :param context: 异常的内容
+    :return:
+    """
+    response = drf_exception_handler(exc, context)
+
+    # 判断是否已经处理完成
+    if not response:
+        # 手动处理
+        logger.error("[%s] %s" % (context['view'], exc))
+        if isinstance(exc, DatabaseError) or isinstance(exc, RedisError):
+            return Response({'error': "服务器内部异常"}, status=507)
+    return response
