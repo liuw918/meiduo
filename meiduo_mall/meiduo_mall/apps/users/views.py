7b29bcd417d68dd6a7a224dab5d2d395c72942f5
liuw918 <yjwfn5656131>
2018-11-19 19:13:51 +0800

    JWT 手动调节认证，可通过手机号和用户名登录

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_jwt.settings import api_settings
+from django.db.models import Q
 
 from .models import User
 from . import serializers
@@ -32,7 +34,36 @@ class MobileCountView(APIView):
         }
         return Response(data)
 
+
 # 指明用户序列化器
 # url(r'^users/$', views.UserView.as_view()),
 class UserView(CreateAPIView):
     serializer_class = serializers.CreateUserSerializer
+
+
+class LoginApiView(APIView):
+    # 用户登录
+    def post(self, requeset):
+        data = requeset.data
+        username = data.get("username")
+        password = data.get("password")
+
+        users = User.objects.filter(Q(username=username) | Q(mobile=username))  # queryset对象，要么有一个，要么没有
+        if users:
+            # 用户存在
+            user = users[0]
+
+            if user.check_password(password):
+                # 颁发token
+                # 生成jwttoken
+                jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+                jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+                payload = jwt_payload_handler(user)
+                token = jwt_encode_handler(payload)
+                return Response({
+                    'username': user.username,
+                    "user_id": user.id,
+                    "token": token
+                })
+        return Response({"messige": "用户名或密码错误"}, status=400)
