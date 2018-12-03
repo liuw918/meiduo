b89a84a774045b50e950c0b5aa762a59256e45a5
liuw918 <yjwfn5656131>
2018-12-03 16:46:50 +0800

    购物车删除

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -170,3 +170,35 @@ class UserCartView(APIView):
             response = Response(data, status=201)
             response.set_cookie('cart', json.dumps(cart), max_age=60 * 60 * 24 * 365)
             return response
+    def delete(self, request):
+        data = request.data
+        sku_id = data['sku_id']
+        try:
+            user = request.user
+        except Exception:
+            # 异常说民用户未登录
+            user = None
+        if user is not None and user.is_authenticated:
+            conn = get_redis_connection('cart')
+            cart_id = 'cart_%d' % user.id
+            cart_selected_id = 'cart_selected_%d' % user.id
+            pipeline = conn.pipeline()
+            # 删除商品
+            pipeline.hdel(cart_id, sku_id)
+            # 移出选中状态
+            pipeline.srem(cart_selected_id, sku_id)
+            pipeline.execute()
+            return Response(status=204)
+        else:
+            # 操作cookie
+            cart_cookie = request.COOKIES.get('cart', None)
+            if cart_cookie:
+                cart = json.loads(cart_cookie)
+            else:
+                cart = {}
+            if str(sku_id) in cart:
+                del cart[str(sku_id)]
+
+            response = Response(status=204)
+            response.set_cookie('cart', json.dumps(cart), max_age=60 * 60 * 24 * 365)
+            return response
